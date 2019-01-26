dc10cd6025740e58e471fb5dfdde7c034e6b5258
da crands <davecrands@gmail.com>
2019-01-25 21:14:55 -0500

    Better landing page, flatter CSS

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import CheckBox from '../assets/images/check-box.svg'; 
 import Book from '../assets/images/book.svg'; 
@@ -20,24 +21,26 @@ export default class Landing extends Component {
       <div>
         <main className="landing">
           <div className="landing--bg"></div>
-          {/* <section>
-            <h3>Cool things you can do...</h3>
-          </section> */}
-          <section>
-            <div>
-              <Book />
+          <section className="box">
+            <Link to="/best" className="box__item">
+              <Book className="box__svg"/>
               <h3>
-                Browse the New York Times Best-Sellers. <br/> Click a book to buy it on Amazon.
+                {/* Browse the New York Times Best-Sellers. <br/> Click a book to buy it on Amazon. */}
+                NYT Best Sellers
               </h3>
-            </div>
-            <div>
-              <Time />
-              <h3>Read articles dating back to the year 1900.</h3>
-            </div>
-            <div>
-              <CheckBox />
+              <p>
+              Browse the New York Times Best-Sellers. Click a book to buy it on Amazon.
+              </p>
+            </Link>
+            <Link to="/archives" className="box__item">
+              <Time className="box__svg" />
+              <h3>Explore History</h3>
+              <p>Read articles dating back to the year 1900.</p>
+            </Link>
+            <Link to="/popular" className="box__item">
+              <CheckBox className="box__svg" />
               <h3>Check out the news that's causing a buzz.</h3>
-            </div>                                    
+            </Link>                                    
           </section>          
         </main>        
       </div>
